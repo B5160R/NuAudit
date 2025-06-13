f93ffed760238bab89aabc8926fe45ab439f9716
B5160R <freidrichson@gmail.com>
2025-06-13 22:04:58 +0200

    Remove unused validation models for allowed licenses and NuGet versions

--- src/Models/ValidationModels/AllowedNuGetVersions.cs
+++
@@ -1,8 +0,0 @@
-namespace Limbo.Test.Helpers.NuGetPackageCheck.Models.ValidationModels;
-public class AllowedNuGetVersions {
-    public static object[] GetTestCases =
-    {
-        new object[] { "Swashbuckle.AspNetCore", "6.6.2", "" },
-        new object[] { "NUnit", "4.2.2", "Limbo.Test.Security"}
-    };
-}
